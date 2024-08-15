4d854f22bbfa4f5ea6bb9a651bfa515a2bd8c8d1
Ogban Ugot <ogbanugot>
2024-08-15 17:35:51 +0100

    add frontend service

---
+++ src/syntax_frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { syntax_backend } from 'declarations/syntax_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    console.log(syntax_backend); // this works now!
+    syntax_backend.greet(name).then((greeting) => { // But Remember greet won't work cause it doesn't exist
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
