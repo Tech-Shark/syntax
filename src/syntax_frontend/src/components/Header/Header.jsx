05f5b9e2ccea010d15d70d86a96d5c8c294f7168
samuel fagbohun <samuelfagbohun041@gmail.com>
2024-11-16 03:09:32 +0100

    added hero section

--- src/syntax_frontend/src/components/Header/Header.jsx
+++ src/syntax_frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../../assets/images/logo_place_holder.svg'
 import build_icon from '../../assets/images/build_icon.svg'
 import { Link } from 'react-router-dom'
+import menu_icon from '../../assets/images/menu_icon.svg'
 
 function Header() {
 
@@ -35,10 +36,10 @@ function Header() {
         },
     ]
   return (
-    <header className='flex justify-center pt-8'>
-    <nav className='flex items-center w-4/5 sm:w-auto rounded-full border px-4 lg:px-[1.25rem] py-[0.5rem] '>
+    <header className='flex justify-center pt-8 z-40'>
+    <nav className='flex items-center w-4/5 md:w-auto rounded-full bg-white px-4 lg:px-[1.25rem] py-[0.5rem] justify-between'>
         <Link to='/'>
-        <img src={logo} alt="logo icon" className='w-8 lg:w-[3.75rem] h-8 lg:h-[3.75rem]  cursor-pointer mr-4 lg:mr-8' />
+        <img src={logo} alt="logo icon" className='w-8 lg:w-[3.75rem] h-8 lg:h-[3.75rem]  cursor-pointer md:mr-4 lg:mr-8' />
         </Link>
     <ul className='hidden md:flex items-center justify-center gap-4 lg:gap-12 '>
         {navLinks.map((link) => (
@@ -60,6 +61,10 @@ function Header() {
         
         <img src={build_icon} alt="build icon" className='w-8 lg:w-[3.75rem] h-8 lg:h-[3.75rem] cursor-pointer group-hover:transition-all group-hover:duration-300 group-hover:ease-in group-hover:w-4 group-hover:lg:w-[2.75rem]  group-hover:lg:h-[2.75rem] group-hover:h-4 group-hover:rotate-45' />
         </div>
+    </nav>
+
+    <nav className='md:hidden block'>
+        <img src={menu_icon} alt="menu icon" className='w-8  h-8  cursor-pointer ' />
     </nav>
 
     </nav>
--- src/syntax_frontend/src/pages/Home/Home.jsx
+++ src/syntax_frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import hero_bg from '../../assets/images/hero_bg.svg'
 
 function Home() {
   return (
-    <section>
-      <div className='px-4' >
+    <section className='relative'>
+      <div className='px-4 h-[62.49rem] bg-[#E1E0F3]    ' >
+        <img src={hero_bg} alt="hero background" className='w-[9.56rem] lg:w-3/5 lg:h-[28.31rem] h-[7.31rem]  absolute left-[-2rem] lg:left-[-12rem] z-0' />
+        <div className='absolute inset-x-0 '>
         <Header />
+        <Hero />
+        </div>
+
     </div>
     </section>
   )
