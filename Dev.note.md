fe98415e36a7f1f201468f385c494ff7b0f0d145
onfranciis <onukwuf@gmail.com>
2024-12-13 23:53:31 +0100

    feat: addsupport for  automatic candid generation

--- Dev.note.md
+++ Dev.note.md
@@ -3,4 +3,17 @@
 - When creating any data structure using the BTreeMap, if there's going to be a candid query to get all elements (as an array of elements), and the elements are going to be "objects" (or structs), you would have to mark the value of the keys as optional in your struct `{key: Option<AnElementStruct>}` . If not, you would get an error from the candid ui when you make a query and it returns an empty vector (most likely when you haven't inserted any data yet).
   Also, don't forget to update the candid file and make the changed fields as optional also.
 
-Experimentally: You could just explicitly return an empty vector if the array returned from the tree is empty.
+> [!WARNING]
+> Experimentally: You could just explicitly return an empty vector if the array returned from the tree is empty.
+
+<br>
+
+- Creating a candid file has never been easier. Now, you don't have to manually update it after creating a new endpoint. Just run
+
+```shell
+ cargo build --release --target wasm32-unknown-unknown --package syntax_backend && candid-extractor target/wasm32-unknown-unknown/release/syntax_backend.wasm > src/syntax_backend/syntax_backend.did
+```
+
+> [!TIP]
+> Also, update the lib.rs file when a new struct for a data called under a query/update is used.
+> CC: [https://internetcomputer.org/docs/current/developer-docs/backend/rust/candid](https://internetcomputer.org/docs/current/developer-docs/backend/rust/candid)
--- src/syntax_backend/src/lib.rs
+++ src/syntax_backend/src/lib.rs
@@ -1,3 +1,32 @@
+// All these imports are relevant to the export_candid macro
+use crate::schema::admin::Admin;
+use crate::schema::admin::AdminInput;
+use crate::schema::admin::AdminResponse;
+use crate::schema::credit::Credit;
+use crate::schema::credit::CreditResponse;
+use crate::schema::cv::AnalysisResult;
+use crate::schema::cv::CVAnalysisResponse;
+use crate::schema::cv::CVResponse;
+use crate::schema::cv::CVUserInput;
+use crate::schema::grammar::GrammarAnalysisResponse;
+use crate::schema::grammar::GrammarCheckResult;
+use crate::schema::grammar::GrammarResponse;
+use crate::schema::grammar::GrammarUserInput;
+use crate::schema::notifications::notification_for_everybody::NotificationForEverybody;
+use crate::schema::notifications::notification_for_everybody::NotificationForEverybodyInput;
+use crate::schema::notifications::notification_for_everybody::NotificationForEverybodyResponse;
+use crate::schema::notifications::notification_for_single_user::IndividualNotificationForSingleUser;
+use crate::schema::notifications::notification_for_single_user::NotificationForSingleUserInput;
+use crate::schema::notifications::notification_for_single_user::NotificationForSingleUserResponse;
+use crate::schema::notifications::notification_for_tier::NotificationForTier;
+use crate::schema::notifications::notification_for_tier::NotificationForTierInput;
+use crate::schema::notifications::notification_for_tier::NotificationForTierResponse;
+use crate::schema::user::User;
+use crate::schema::user::UserInput;
+use crate::schema::user::UserResponse;
+use ic_cdk::api::management_canister::http_request::HttpResponse;
+use ic_cdk::api::management_canister::http_request::TransformArgs;
+
 pub mod schema;
 pub mod service;
 pub mod storage;
@@ -14,3 +43,6 @@ pub const NOTIFICATION_FOR_PREMIUM_MEMORY_ID: u8 = 37;
 pub const NOTIFICATION_FOR_FREEMIUM_MEMORY_ID: u8 = 41;
 pub const QUOTA_ERROR: &str = "quota error";
 pub const MONTHLY_TRIAL_ERROR: &str = "You have exhausted your limit for the month!";
+
+// Enable Candid export
+ic_cdk::export_candid!();
