59e300cd5a775a59d6f8f5984340a36a1c0e0aff
onfranciis <onukwuf@gmail.com>
2024-12-26 21:05:04 +0100

    feat (breaking): reset frontend canister

--- src/syntax_frontend/src/App.tsx
+++ src/syntax_frontend/src/App.tsx
@@ -1,76 +1,17 @@
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { store } from "./redux/store";
-
-import Achievements from "./pages/Achievements/Achievements";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Education from "./pages/Education/Education";
-import Home from "./pages/Home/Home";
-import PersonalInfo from "./pages/PersonalInfo/PersonalInfo";
-import Portfolio from "./pages/Portfolio/Portfolio";
-import SavedInfo from "./pages/SavedInfo/SavedInfo";
-import Skills from "./pages/Skills/Skills";
-import UploadCv from "./pages/UploadCV/UploadCv";
-import WelcomeScreen from "./pages/WelcomeScreen/WelcomeScreen";
-import WorkExperience from "./pages/WorkExperience/WorkExperience";
 
 function App() {
   return (
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>Home component should be here</>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
 
 export default App;
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/welcome",
-    element: <WelcomeScreen />,
-  },
-  {
-    path: "/upload-cv",
-    element: <UploadCv />,
-  },
-  {
-    path: "/personal-info",
-    element: <PersonalInfo />,
-  },
-  {
-    path: "/work-experience",
-    element: <WorkExperience />,
-  },
-  {
-    path: "/skills",
-    element: <Skills />,
-  },
-  {
-    path: "/education",
-    element: <Education />,
-  },
-  {
-    path: "/achievements",
-    element: <Achievements />,
-  },
-  {
-    path: "/portfolio",
-    element: <Portfolio />,
-  },
-  {
-    path: "/saved_info",
-    element: <SavedInfo />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "*",
-    element: <></>,
-  },
-]);
--- src/syntax_frontend/src/main.tsx
+++ src/syntax_frontend/src/main.tsx
@@ -1,9 +1,7 @@
-// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import "./main.css";
+import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
