8c5d171e9e4140b741eb22064f8eaf6d5012c802
Akem-Ben <akemini.ndaobong@gmail.com>
2025-01-10 16:46:45 +0100

    Cv Templates

--- src/syntax_frontend/src/App.jsx
+++ src/syntax_frontend/src/App.jsx
@@ -13,8 +13,28 @@ import Portfolio from './pages/Portfolio/Portfolio';
 import SavedInfo from './pages/SavedInfo/SavedInfo';
 import DashboardUser from './pages/Dashboard/DashboardUser';
 import DashboardAdmin from './pages/Dashboard/DashboardAdmin';
-import Template1 from './components/ResumeTemplates/Template1';
-import Template2 from './components/ResumeTemplates/Template2';
+import Template1 from './components/cvTemplates/Template1';
+import Template2 from './components/cvTemplates/Template2';
+import Template3 from './components/cvTemplates/Template3';
+import Template4 from './components/cvTemplates/Template4';
+import Template5 from './components/cvTemplates/Template5';
+import Template6 from './components/cvTemplates/Template6';
+import Template7 from './components/cvTemplates/Template7';
+import Template8 from './components/cvTemplates/Template8';
+import Template9 from './components/cvTemplates/Template9';
+import Template10 from './components/cvTemplates/Template10';
+import Template11 from './components/cvTemplates/Template11';
+import Template12 from './components/cvTemplates/Template12';
+import Template13 from './components/cvTemplates/Template13';
+import Template14 from './components/cvTemplates/Template14';
+import Template15 from './components/cvTemplates/Template15';
+import Template16 from './components/cvTemplates/Template16';
+import Template17 from './components/cvTemplates/Template17';
+import Template18 from './components/cvTemplates/Template18';
+import Template19 from './components/cvTemplates/Template19';
+import Template20 from './components/cvTemplates/Template20';
+import Template21 from './components/cvTemplates/Template21';
+import Template22 from './components/cvTemplates/Template22';
 
 const router = createBrowserRouter([
   {
@@ -65,6 +85,8 @@ const router = createBrowserRouter([
     path: "/admin_dashboard",
     element: <DashboardAdmin />
   },
+
+  //=======CV Templates, Just for views. To be deleted after proper integration===========//
   {
     path: "/temp1",
     element: <Template1 />
@@ -72,6 +94,87 @@ const router = createBrowserRouter([
   {
     path: "/temp2",
     element: <Template2 />
+  },
+  {
+    path: "/temp3",
+    element: <Template3 />
+  },
+  {
+    path: "/temp1",
+    element: <Template4 />
+  },
+  {
+    path: "/temp1",
+    element: <Template5 />
+  },
+  {
+    path: "/temp1",
+    element: <Template6 />
+  },
+  {
+    path: "/temp1",
+    element: <Template7 />
+  },
+
+  {
+    path: "/temp1",
+    element: <Template8 />
+  },
+  {
+    path: "/temp1",
+    element: <Template9 />
+  },
+  {
+    path: "/temp1",
+    element: <Template10 />
+  },
+  {
+    path: "/temp1",
+    element: <Template11 />
+  },
+  {
+    path: "/temp1",
+    element: <Template12 />
+  },
+  {
+    path: "/temp1",
+    element: <Template13 />
+  },
+  {
+    path: "/temp1",
+    element: <Template14 />
+  },
+  {
+    path: "/temp1",
+    element: <Template15 />
+  },
+  {
+    path: "/temp1",
+    element: <Template16 />
+  },
+  {
+    path: "/temp1",
+    element: <Template17 />
+  },
+  {
+    path: "/temp1",
+    element: <Template18 />
+  },
+  {
+    path: "/temp1",
+    element: <Template19 />
+  },
+  {
+    path: "/temp1",
+    element: <Template20 />
+  },
+  {
+    path: "/temp1",
+    element: <Template21 />
+  },
+  {
+    path: "/temp1",
+    element: <Template22 />
   },
   
 ]);
