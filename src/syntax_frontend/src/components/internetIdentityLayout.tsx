3b306a8762c4f8b914dc4332dda92d7ff928676e
Emmaibe <emmanuelibenwankwo@gmail.com>
2025-02-12 21:01:32 +0100

    updatea

--- src/syntax_frontend/src/components/internetIdentityLayout.tsx
+++ src/syntax_frontend/src/components/internetIdentityLayout.tsx
@@ -5,11 +5,12 @@ import WelcomeDescription from "./welcomeDescription";
 import { NextButton } from "./welcomeNavButtons";
 import welcomeHeroBanner from "../assets/images/welcomeHerobanner.svg";
 import welcomeVideo from "../assets/images/Create Your Internet Identity in 2 Minutes.mp4"
-import { useUser } from "../contexts/AuthContext";
+// import { useUser } from "../contexts/AuthContext";
+import {useAuth} from "@/contexts/AuthenticationContext";
 
 const InternetIdentityLayout: React.FC = () => {
 
-  const { login } = useUser();
+  const { isAuth, login, logout } = useAuth();
 
   // const { handleSignup } = useUser();
   // const [formData, setFormData] = useState<any>({
@@ -49,14 +50,14 @@ const InternetIdentityLayout: React.FC = () => {
        <section>
         <WelcomeHeader leftLabel="Back" />
         <div className="flex justify-center items-center flex-col h-auto w-full pt-32 pr-[0.8125rem] pb-0 pl-3.5 gap-14">
-          
+
           {/* Welcome Description and Toggle Buttons */}
           <div className="flex flex-col justify-center items-center gap-[1.8rem]">
             <WelcomeDescription
               heading="New to Internet Identity?"
               subheading="Follow these steps to create an account or learn how to use your Internet Identity."
             />
-            
+
             {/* Toggle Buttons */}
             <div className="flex items-center gap-2 py-2 px-3 bg-[#E1E0F3] rounded-[0.25rem]">
               {/* Video Recording Button */}
@@ -68,7 +69,7 @@ const InternetIdentityLayout: React.FC = () => {
               >
                 Video Rec.
               </button>
-              
+
               {/* Screenshot Button */}
               <button
                 onClick={() => setView('screenshot')}
@@ -85,8 +86,8 @@ const InternetIdentityLayout: React.FC = () => {
           <div className="h-80 w-full lg:w-[43rem] flex justify-center items-center bg-gray-100 rounded-md shadow-md p-3">
             {view === 'video' ? (
               <div className="border-purple-500 ">
-                <video 
-                  controls 
+                <video
+                  controls
                   className="w-full h-80"
                 >
                 <source src={welcomeVideo} type="video/mp4" />
@@ -95,9 +96,9 @@ const InternetIdentityLayout: React.FC = () => {
                 </video>
               </div>
             ) : (
-              <img 
-                src={welcomeHeroBanner} 
-                alt="Screenshot" 
+              <img
+                src={welcomeHeroBanner}
+                alt="Screenshot"
                 className="max-w-full max-h-full object-contain"
               />
             )}
